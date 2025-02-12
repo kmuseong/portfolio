e67a2c1b070aca11ad7534ea7a35baea59684b12
kim-museong <antjd0419@gmail.com>
2025-02-12 23:28:13 +0900

    FEATURE : 로딩화면 적용

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,25 @@ import ProjectDetailPage from './pages/ProjectDetailPage';
 import Layout from './components/Layout';
 import AboutPage from './pages/AboutPage';
 import ResumePage from './pages/ResumePage';
+import LoadingScreen from './components/LoadingScreen';
+import { useState } from 'react';
 
 const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
     return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/resume" element={<ResumePage />} />
-                <Route path="/projects" element={<ProjectsPage />} />
-                <Route path="/projects/:projectName" element={<ProjectDetailPage />} />
-            </Route>
-        </Routes>
+        <>
+            {isLoading && <LoadingScreen onComplete={() => setIsLoading(false)} />}
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/resume" element={<ResumePage />} />
+                    <Route path="/projects" element={<ProjectsPage />} />
+                    <Route path="/projects/:projectName" element={<ProjectDetailPage />} />
+                </Route>
+            </Routes>
+        </>
     );
 };
 
