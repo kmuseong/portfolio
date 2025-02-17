86a673f59a8aefa5a136bca5ee6093d8034985d0
kim-museong <antjd0419@gmail.com>
2025-02-17 23:51:20 +0900

    FEATURE(theme) : 테마 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { ProjectProvider } from './hooks/ProjectContext.jsx';
+import { ProjectProvider } from './context/ProjectContext.jsx';
+import { ThemeProviderWrapper } from './context/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter basename="/portfolio">
-        <ProjectProvider>
-            <App />
-        </ProjectProvider>
+        <ThemeProviderWrapper>
+            <ProjectProvider>
+                <App />
+            </ProjectProvider>
+        </ThemeProviderWrapper>
     </BrowserRouter>
 );
