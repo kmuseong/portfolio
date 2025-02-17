5c88da57c838ddb80161b22b51a3a15cbcee53bf
kim-museong <antjd0419@gmail.com>
2025-02-17 23:53:25 +0900

    FEATURE(theme) : 테마버튼 구현

---
+++ src/components/ThemeButton.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { useTheme } from '../context/ThemeContext';
+import { Moon, Sun } from 'lucide-react';
+
+const Button = styled.button`
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.text};
+    border: none;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        opacity: 0.8;
+    }
+`;
+const ThemeButton = () => {
+    const { isDarkMode, setIsDarkMode } = useTheme();
+
+    return (
+        <Button onClick={() => setIsDarkMode((prev) => !prev)}>
+            {isDarkMode ? <Moon strokeWidth={1} size={30} /> : <Sun strokeWidth={1} size={30} />}
+        </Button>
+    );
+};
+
+export default ThemeButton;
